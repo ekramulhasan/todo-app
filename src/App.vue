ed85334113e0443ecf64a3fe2797810c4862c589
ekramul <ekramulshawon1@gmail.com>
2024-04-30 23:00:42 +0600

    todo-app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga incidunt ipsa ducimus dolorum assumenda. Voluptatum, delectus reprehenderit aut eveniet, repellendus facere veniam fugiat et, soluta sequi recusandae maxime unde odit.</h1>
+
+</template>
+
+<style scoped>
+
+
+</style>
