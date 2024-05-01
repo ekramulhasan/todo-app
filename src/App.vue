13bd8a4fa6723a5a5a25cbb1cc7ac43178299bfb
ekramul <ekramulshawon1@gmail.com>
2024-05-01 10:55:28 +0600

    ui design and axios install

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,48 @@
 
 <template>
 
-    <div class="row">
-        <div class="col-10 m-auto mt-5">
-            <div class="card">
-                <div class="card-body">
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-md-12">
 
-                    <h1>hello world</h1>
+                <div class="card">
+                    <div class="card-body">
 
+                        <h4 class="card-title text-center"> Task List</h4>
+
+                        <div class="row mb-3">
+
+                            <div class="col-md-12">
+                                <form action="">
+                                    <label for="" class="form-label">Add New Task</label>
+                                    <input type="text" class="form-control">
+
+                                    <button type="submit" class="btn btn-success mt-3">Add task</button>
+                                </form>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="card-body">
+                        <h4 class="text-primary">Task Item</h4>
+                        <hr>
+
+                        <ul class="list-group mb-3">
+
+                            <li class="list-group-item d-flex justify-content-between">added new task <a href=""
+                                    class="text-danger"> <i class="fa-solid fa-xmark"></i></a></li>
+
+                        </ul>
+                    </div>
                 </div>
+
             </div>
         </div>
+
     </div>
 
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
